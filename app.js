24d9f39fce2dab628c9555c95d3b2ccd794858ea
mihnhyuk <mihnhyuk@naver.com>
2022-08-18 15:59:53 +0900

    blog main page

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 var express = require('express');
+var fs = require('fs');
 var blog_address = require('./router/blog-address.js');
 var app = express();
 
-app.use('./{blog-address}', blog_address);
+//app.use('/', blog_address);
+
+app.get('/', (req, res) =>{
+    res.send('home');
+})
+
+app.get('/:address', (req, res) => {
+    fs.readFile("./data/blog-main.json", "utf8", (err, data1) => {
+        if (err) {
+          console.log("File read failed:", err);
+          res.send("error");
+          return;
+        }
+        fs.readFile("./data/post-list.json", "utf8", (err, data2) => {
+			if (err) {
+				console.log("File read failed:", err);
+				res.send("error");
+				return;
+			}
+			var ret = JSON.parse(data1);
+			var post = JSON.parse(data2);
+			ret.postList = post[ret.userAddress]
+			res.status(200).json(ret);
+		})
+      });
+})
 
 app.listen(3000, function () {
     console.log('Connected port 3000');
--- router/blog-address.js
+++ router/blog-address.js
@@ -1,6 +1,21 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
+var router = express.Router();
 
-app.get('/', (req, res) => {
-    //블로그 메인
+router.get('/', (req, res) =>{
+    res.send('home');
 })
+
+router.get('/:address', (req, res) => {
+    fs.readFile("./data/blog-main.json", "utf8", (err, data) => {
+        if (err) {
+          console.log("File read failed:", err);
+          res.send("error");
+          return;
+        }
+        console.log("File data:", data);
+        res.send("dynamic working");
+      });
+})
+
+module.exports = router;
