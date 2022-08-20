94733fae0a0ce0e9280c6767bec5d218bb3ea367
mihnhyuk <mihnhyuk@naver.com>
2022-08-21 02:56:41 +0900

    데이터 일부 수정, 작은 오류 정리

--- src/comment/controller/index.js
+++ src/comment/controller/index.js
@@ -1,7 +1,7 @@
-var appendComment = require('./appendComment');
+var appendComment = require('..sevice/appendComment');
 var commentLike = require('./commentLike');
 var getComment = require('./getComment');
-var getTime = require('./getTime');
+var getTime = require('../sevice/getTime');
 var postComment = require('./postComment');
 
 module.exports = {appendComment, commentLike, getComment, getTime, postComment}
--- src/comment/controller/postComment.js
+++ src/comment/controller/postComment.js
@@ -1,8 +1,6 @@
 var qs = require('querystring')
 var append = require('./appendComment')
-var time = require('./getTime')
-
-
+var time = require('../sevice/getTime')
 
 var postComment = (request, response) => {
     var body = request.body
@@ -10,6 +8,8 @@ var postComment = (request, response) => {
 	var content = body.content;
 	var ret = {
 		"comment-id" : (Math.floor(Math.random() * 100)).toString(10),
+		"blogName" : request.params.address,
+		"postID" : request.params.postID,
 		"user-id" : user,
 		"icon" : "img",
 		"content" : content,
@@ -19,7 +19,7 @@ var postComment = (request, response) => {
 		"like-num" : 0
 	}
 	append(ret)
-	response.send("hello1")
+	response.status(200).json(ret)
 }
 
 module.exports = postComment;
