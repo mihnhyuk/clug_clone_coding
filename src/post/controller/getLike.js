fd14b38503a1fe036c1c0b2e75bdfd03727d163c
mihnhyuk <mihnhyuk@naver.com>
2022-08-21 18:17:18 +0900

    오류 수정

--- src/post/controller/getLike.js
+++ src/post/controller/getLike.js
@@ -8,16 +8,15 @@ var getLike = (req, res) => {
 			return;
 		}
         var params = req.params;
-		var ret = JSON.parse(data)//[params.address.postID]//[params.postID];
-		ret["likes"]++ ;
+		var ret = JSON.parse(data)
+		ret[req.params.address][req.params.postID]["likes"]++ ;
         console.log(ret);
-        res.status(200).json(ret);
 		var json = JSON.stringify(ret); //convert it back to json
 		fs.writeFile('./data/post.json', json, 'utf8', (err) => {
 			if (err){
 				console.log("write error");
 			}
-			res.status(200);
+			res.status(200).json(ret);
 		});
 	})	
 }
