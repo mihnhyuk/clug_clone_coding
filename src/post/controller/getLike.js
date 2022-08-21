c582edd05655f667b3a9f92f691bb4ec510939e2
causyj <jainayj804998@gmail.com>
2022-08-21 09:33:00 +0900

    postlike

---
+++ src/post/controller/getLike.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var getLike = (req, res) => {
+	fs.readFile("./data/post.json", "utf8", (err, data) => {
+		if (err) {
+			
+			console.log("File read failed:", err);
+			res.send("error");
+			return;
+		}
+		var ret = JSON.parse(data);
+		var com = ret.data.find(comm => comm["likes"] === req.params.comID);
+		com["likes"]++ ;
+		console.log(ret);
+		var json = JSON.stringify(ret); //convert it back to json
+		fs.writeFile('./data/post.json', json, 'utf8', (err) => {
+			if (err){
+				console.log("write error");
+			}
+			res.status(200);
+		});
+	})	
+}
+
+
+module.exports = getLike;
--- src/post/router/router.js
+++ src/post/router/router.js
@@ -1,9 +1,10 @@
 var express = require('express');
 const router = express.Router({ mergeParams: true }); 
 var getUser = require('../controller/getPost');
+var getLike = require('../controller/getLike');
 
 router.get('/:postID', getUser)
-// router.get('/:postID/like',)
+router.get('/:postID/like', getLike);
 
 
 module.exports = router;
